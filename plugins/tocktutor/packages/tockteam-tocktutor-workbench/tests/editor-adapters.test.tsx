e3a7aba9519ea96c4fa63e5348cdf8a9c08420d0
taowang <twang293@gmail.com>
2026-09-10 16:56:02 -0700

    fix(tocktutor): brighten source heading markers

--- plugins/tocktutor/packages/tockteam-tocktutor-workbench/tests/editor-adapters.test.tsx
+++ plugins/tocktutor/packages/tockteam-tocktutor-workbench/tests/editor-adapters.test.tsx
@@ -33,6 +33,7 @@ describe('CodeMirror Source editor', () => {
     expect(sourceEditor.className).toContain('[&_.cm-gutters]:hidden')
     expect(sourceEditor.className).toContain('[&_.cm-activeLine]:bg-transparent')
     expect(sourceEditor.className).toContain('[&_.cm-tock-heading-mark]:!text-inherit')
+    expect(sourceEditor.className).toContain('[&_.cm-tock-heading-mark_*]:!text-inherit')
     expect(sourceEditor.className).toContain('[&_.cm-tock-heading-mark]:[font-size:inherit]')
     expect(sourceEditor.className).not.toContain('[&_.cm-tock-heading-mark]:!text-[var(--tt-muted)]')
     expect(screen.getByLabelText('Markdown Source Editor').className).toContain('[&_.cm-scroller]:leading-6')
